8aa423914f341c773dac901d37b8117a33efd535
David Fridrich <fridrich.david19@gmail.com>
2021-07-16 22:50:19 +0200

    script for log init

---
+++ make_log_pretty.py
@@ -0,0 +1,35 @@
+import os
+
+
+def exportLogFileToExcel(file):
+    """Export log file into excel-like files or something (tbdl - to be determined later)
+
+    """
+    pass
+
+def mainloop():
+    #check if log exists
+
+    #open file
+    
+    #main for loop
+        #ignore lines that start with '-' & first line (optimize this somehow)
+
+        #possibilities:
+            #one:
+            #two for loops, first to find out what numbers to use
+            #second to apply
+
+            #two:
+            #have predetermined values, if any string reaches over, call mainloop again
+            
+            #three:
+            #have predetermined values, if any string is longer, cut it(probably easiest)
+
+            #have a possibility of exporting this into excel-like sheet or something as well
+
+    #close file
+    pass
+
+if __name__ == "__main__":
+    mainloop()
